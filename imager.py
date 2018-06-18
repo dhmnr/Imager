4dcf948bd1f886a292864ff0e6196d0520359bff
Daem0n-th <dheemanthmanur72@gmail.com>
2018-06-19 02:51:40 +0530

    Add files via upload

---
+++ imager.py
@@ -0,0 +1,100 @@
+#!/bin/python3
+from PIL import Image
+from math import ceil,sqrt
+import binascii
+import argparse
+
+def pad_hex(string):
+	length=len(string)
+	if(length%6==0):
+		return string
+	else:
+		padding='0'*(6-(length%6))
+		return padding+string
+
+def pad_image(tuple_list):
+	length=len(tuple_list)
+	n=sqrt(length)
+	padding=[(0,0,0)]*((ceil(n)**2)-length)
+	return padding+tuple_list
+
+def hex2tuple(hex_color):
+	r,g,b=[int(hex_color[i:i+2],16) for i in range(0,len(hex_color),2)]
+	return (r,g,b)
+
+def tuple2hex(pixel):
+	hex_rgb=[hex(i)[2:] for i in pixel]
+	hex_rgb=[('0'*(2-len(i)))+i for i in hex_rgb]
+	return ''.join(hex_rgb)
+	
+def get_offset(offset_tuple):
+	offset = tuple2hex(offset_tuple)
+	for j in range(6):
+		if(offset[j]=='0'):
+			j=j+1
+		else:
+			break
+	return offset[j:]
+
+def encode(src_fp,dest_fp):
+	print("Reading from file...")
+	hex_str=pad_hex(binascii.hexlify(src_fp.read()).decode('utf-8'))
+	hex_colors=pad_image([hex2tuple(hex_str[i:i+6]) for i in range(0,len(hex_str),6)])
+	length=int(sqrt(len(hex_colors)))
+	src_fp.close()
+	resolution=(length,length)
+	print("Resolution of image is {}x{}".format(length,length))
+	img=Image.new('RGB',resolution,'white')
+	img.putdata(hex_colors)
+	try:
+		img.save(dest_fp)
+	except ValueError:
+		print("{} is not a valid filename. Defaulting to PNG.\nSupported formats are BMP,JPEG,JPG,PNG,TIFF".format(out))
+		fp2.close()
+		fp2=open(out+'.png','wb')
+		img.save(dest_fp)
+	print("Done!")
+
+def decode(src_fp,dest_fp):
+	i=0
+	print("Reading from Image...")
+	img=Image.open(src_fp)
+	color_list=list(img.getdata())
+	while(color_list[i]==(0,0,0)):
+		i=i+1
+	offset = get_offset(color_list[i])
+	print("Writing to binary file...")
+	hex_list=offset + ''.join([tuple2hex(pixel) for pixel in color_list[i+1:]])
+	file_data=binascii.unhexlify(hex_list)
+	dest_fp.write(file_data)
+	dest_fp.close
+	src_fp.close
+	print("Done!")
+
+def main(In,out,d):
+	
+	try:
+		fp1=open(In,'rb')
+	except FileNotFoundError:
+		print("No such file is found.\nExiting...")
+		return
+	
+	try:
+		fp2=open(out,'wb')
+	except FileNotFoundError:
+		print("Destination not found.\nExiting...")
+		return
+	if(d):
+		decode(fp1,fp2)
+	else:
+		encode(fp1,fp2)
+	
+if __name__=="__main__":
+	parser = argparse.ArgumentParser(description = "Encode any file to an image and decode .\n Supported Image formats are BMP,JPEG,JPG,PNG,TIFF")
+	parser.add_argument('-f','--file' ,required=True ,help="Input binary file or Image file in case of decoding")
+	parser.add_argument('-o','--out' ,required=True,help="Output Image/binary file destination with file extension.")
+	parser.add_argument('-d','--decode',action="store_true",default=False,help="Use this to decode the image into a binary file")
+	args = parser.parse_args()
+	main(args.file,args.out,args.decode)
+	
+
